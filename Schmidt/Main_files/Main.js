30ac04daacdbbf83f97acb1e786e4715c3110674
kasperschmidt <kschmidt@physics.ucsb.edu>
2016-10-06 21:04:49 +0200

    Update contact info, software list and travel info

--- Schmidt/Main_files/Main.js
+++ Schmidt/Main_files/Main.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20160306
+// Created by iWeb 3.0.4 local-build-20161006
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.69,offset:1.00})});registry.applyEffects();}
